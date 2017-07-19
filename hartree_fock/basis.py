e31afb9d9f6d6870cf5537a2b36ba922f65be715
Gary Zeri <gzeri@c5l1.ps.uci.edu>
2017-07-19 13:10:51 -0700

    adds start of overlap matrix

--- hartree_fock/basis.py
+++ hartree_fock/basis.py
@@ -1,31 +1,54 @@
 #contains functions for constructing hartree basis functions
-import constants
+import constants as consts
+import math
 
 ###############################################
 class basis:
     
-    def constructBasis(atomicNumbers, coord):
-    #build basis set for each atom
+    def constructBasis(self, atomicNumbers, coord):
+        #build basis set for each atom
 
-    basis = []
+        basis = []
 
-    for atom in range(len(atomicNumbers)):
-	basis.append([])
-	for alpha in const.alpha[atom]:
-            basis["gaussian"][atom].append(sOrbital(coord[atomicNumbers[atom]]),alpha)
+        for atom in range(len(atomicNumbers)):
+            basis.append([])
+            for alpha in consts.alpha[atom]:
+                basis[atom].append(self.sOrbital(coord[atom],alpha))
+
+        return basis
 
 ###############################################
-   def sOrbital(coord, alpha):
+    def sOrbital(self, coord, alpha):
    #return s orbital dictionary 
-      orbital = {
+	orbital = {
           
-          "coord":coord
-	  "alpha":alpha
-          "amplitude":((2*alpha)/math.pi)**(0.75)
+            "coord":coord,
+	    "alpha":alpha,
+            "amplitude":((2*alpha)/math.pi)**(0.75)
 }      
 
-     return orbital
+	return orbital
 
 ###############################################
-#ef overlap():
-#construct ovelap matrix 
+    def overlap(self,basis):
+    #constructs overlap function
+    
+
+###############################################
+    def guasProduct(alpha1, alpha2, x1, x2):
+    #use guassian product theorem to return product of two guassians
+    
+    p = alpha1 + alpha2
+    q = (alpha1*alpha2)/p
+    
+    P = ((alpha1** x1) + (alpha2**x2))/p
+    Q = x1 - x2
+ 
+    return math.exp(-q*(Q**2))
+###############################################
+   def gausOverlap(gaus1,gaus2):
+       
+        E = []  
+      
+	for dimension in range(3):
+	     E.append(guasProduct()) 
