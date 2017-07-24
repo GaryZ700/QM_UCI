faae6c825eeabe9b365fa6f1b60dca453de5674f
Gary <gzeri@uci.edu>
2017-07-24 10:55:17 -0700

    adds improved SCF loop

--- 1dfock.py
+++ 1dfock.py
@@ -6,13 +6,14 @@ import numpy as np
 #init main variables
 
 #number of particles 
-N = 3
+N = 2
 
 #alphas and sigmas for each particles
 basisGaus = {
 
-        "alphas":[-2.0,3.0,1.3],
-        "sigmas":[0.5,0.6,0.5]
+        "mus":[0.5,0.0],
+        "sigmas":[0.5,0.6],
+        "alphas":[0.0,0.0]
 }
 
 #size of grid for calculation
@@ -33,17 +34,24 @@ S = []
 #Kinetic Energy Matrix
 T = []
 
+#number of cycles for SCF loop
+maxCycles = 300
 
-
-gausWidthConst = 2.355
-dx = 0.01
+#difference between fock energies at which energy has converged
+convergenceCriteria = 0.0001
 
 ###########################################
 #init important functions
-def basis(position, alpha, sigma):
+def r(igrid):
+    #returns position of particle with respect to box dimensions from grid point
+
+    return igrid * Rx
+
+###########################################
+def basis(position, mu, sigma, alpha):
     #all basies consist of a single guassian
 
-    gaus = math.exp((-((position - alpha)**2))/(2*sigma**2))
+    gaus = math.exp((-((position - alpha)**2) * alpha))
 
     return gausC(sigma)*gaus
 
@@ -60,79 +68,121 @@ def overlap(N, basisSet):
 
     for n in range(N):
         S.append([])
+        print(n)
         for n1 in range(N):
             S[n].append(gausOverlap(basisSet, n, n1))
 
-    return S
+    return np.asarray(S)
+
+###########################################
+def density(eValues, eVectors):
+    #returns density of fock matrix, or guess matrix
+    D = []
+
+    return np.asarray(D)
+
+###########################################
+def exchange():
+    ex = 0.0
+
+    return ex
+
+###########################################
+def KE(N, basisSet):
+    #builds kinetick energy matrix for hamiltonian
+    
+    T = []
+
+    for n in range(N):
+        T.append([])
+        for n1 in range(N):
+                
+            Px = (basisSet["alphas"][n] * basisSet["mus"][n] + basisSet["alphas"][n1] * basisSet["mus"][n1]) / (basisSet["alphas"][n] + basisSet["alphas"][n1])
+
+            t = 0.0
+            t += -2 * basisSet["alphas"][n1] * gausOverlap(basisSet, n, n1)
+            t += 2 * (basisSet["alphas"][n1]**2) * ((Px - basisSet["mus"][n])**2) * gausOverlap(basisSet, n, n1) 
+
+            T[n].append(t)
+    
+    return np.asarray(T)
 
 ###########################################
 def gausOverlap(basisSet, n, n1):
     #uses gaussian product theory to find overlap between two particles
     #http://www.tina-vision.net/docs/memos/2003-003.pdf
-    #is numerically implemented
-    
-    #find width at mid height for both gaussians
-#    W = basisSet["sigmas"][n] * gausWidthConst * 20
-#    W1 = basisSet["sigmas"][n1] * gausWidthConst * 20
-#    
-#    Xr = basisSet["alphas"][n] + W 
-#    X1r = basisSet["alphas"][n1] + W
-#
-#    Xl = basisSet["alphas"][n] - W 
-#    X1l = basisSet["alphas"][n1] - W
-#
-#    #find which gaussian is most extends most to the right, and which to the left
-#    
-#    #if(basisSet["alphas"][n] == basisSet["alphas"][n1] and basisSet["sigmas"][n] == basisSet["sigmas"][n1]):
-#     #   return 1
-#    
-#    if(Xr > X1r):
-#        Xmax = Xr + 1.0
-#    else: 
-#        Xmax = X1r + 1.0
-#
-#    if(Xl < X1l):
-#        x = Xl -1.0
-#    else:
-#        x = X1r - 1.0
-#    
-#    Ymax = -float("inf")
-#
-#    print(x)
-#    print(Xmax)
-#
-    x = -100
-    Ymax = -float("inf")
 
-    #look for max of product gaussian, which is equal to the overlap between the two guassians
-    while(x<100):
-
-        gausProduct = (basis(x, basisSet["alphas"][n],basisSet["sigmas"][n]) * basis(x, basisSet["alphas"][n1],basisSet["sigmas"][n1])) / (gausC(basisSet["sigmas"][n]) * gausC(basisSet["sigmas"][n1]))    
+        p = basisSet["alphas"][n] + basisSet["alphas"][n1]
+        q = (basisSet["alphas"][n] * basisSet["alphas"][n1]) / p
+        Q = basisSet["mus"][n] - basisSet["mus"][n1]
         
-        print(gausProduct)
+        return math.exp(-q*Q**2)
+        
+  #numerical implementation  
+  #  x = -100
+  #  Ymax = -float("inf")
 
-        if(gausProduct > Ymax):
-            Ymax = gausProduct
+  #  #look for max of product gaussian, which is equal to the overlap between the two guassians
+  #  while(x<100):
 
-        x += dx
+  #      gausProduct = (basis(x, basisSet["alphas"][n],basisSet["sigmas"][n]) * basis(x, basisSet["alphas"][n1],basisSet["sigmas"][n1])) / (gausC(basisSet["sigmas"][n]) * gausC(basisSet["sigmas"][n1]))    
+  #      
+  #      print(gausProduct)
 
-    return Ymax
-    
+  #      if(gausProduct > Ymax):
+  #          Ymax = gausProduct
+
+  #      x += dx
+
+  #  return Ymax
+  #  
 ###########################################
-def SCF(F, H, S, N):
+def SCF(F, T, S, N):
     
-    E = 0.0
+    E = []
     
     eValues, eVectors = eig(S)
 
-    #build Matrix to trasform fock matrix
+    #build Matrix to trasform fock matrix from mo basis
+    #to orto basis
     X = eVectors * eValues**(-0.5) * np.transpose(eVectors)
-   
+
+    #guess starting MO coeffs
+    guessVal, guessVec = eig(np.transpose(X) * T * X)
+
+    D = density(guessVal, guessVec)
+
+
     print("####################3")
 
-    print(X)
+    print(guess)
 
-    return E
+    #main scf loop
+    nCycle = 0
+    while(nCycle < maxCycle):
+        
+        Exc = exchange(D)
+
+        F = T + G
+
+        Fprime = np.transPose(X) * F * X
+
+        fValues, fVectors = eig(F)
+
+        fValues = X * fValues
+
+        D = density(fValues, fVectors)
+
+        E.append(fockEnergy(D, T, F))
+
+        if(E[nCycle] - E[ncycle-1] <= convergenceCriteria):
+            print("SCF converged!")
+            return F, E[nCycle]
+        else:
+            print("SCF did not converge during cycle" + str(nCycle))
+
+    print("SCF did not converge!!!")
+    return F, E[nCycle]
 
 ###########################################
 def eig(matrix):
@@ -150,14 +200,20 @@ def eig(matrix):
 ###########################################
 #main code goes here
 
+#calculate alphas for gaussians
+for n in range(N):
+    basisGaus["alphas"][n] = 1/(2 * basisGaus["sigmas"][n]**2)
+
 #construct main matricies
 S = overlap(N, basisGaus)
+T = KE(N, basisGaus)
 
 print(np.asarray(S))
 
-#for igrid in range(ngrid):
 
 SCF(F, T, S, N)
+
+
     
     
 
